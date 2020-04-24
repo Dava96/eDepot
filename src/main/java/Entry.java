db5a565c4814fe25ddd767d87a26f6c082490bfb
kebea <kebea@desktop-3ttrcb3.home>
2020-04-24 20:38:05 +0100

    Kyles comments and Documentation with misc fixes

--- src/main/java/Entry.java
+++ src/main/java/Entry.java
@@ -1,3 +1,9 @@
+/**
+ *
+ * @author Kyle Ellison-Beattie
+ * @author David Lomath-Connis
+ * This class is the entry point for our program and contains our main method.
+ */
 public class Entry
 {
 	public static void main(String args[]) throws Exception {
--- src/main/java/Tanker.java
+++ src/main/java/Tanker.java
@@ -1,12 +1,29 @@
-
-
+/**
+ *
+ * @author Kyle Ellison-Beattie
+ * @author David Lomath Connis
+ *
+ * This class inherits all attributes and methods from vehicle and is used
+ * to define tanker objects.
+ *
+ */
 public class Tanker extends Vehicle
 {
 	private int liquidCapacity;
 	private String liquidType;
 	private String name;
 
-	public Tanker(String make, String model, Integer weight, String regNo, String depot, Integer liquidCapacity, String liquidType) {
+	/**
+	 * This method is a constructor for a tanker object.
+	 * @param make The make of the tanker
+	 * @param model The model of the tanker
+	 * @param weight The weight of the tanker
+	 * @param regNo The registration number of the tanker
+	 * @param depot What depot the tanker is assigned to
+	 * @param liquidCapacity The capacity of the tanker
+	 * @param liquidType The liquid type the tanker holds
+	 */
+	public Tanker(String make, String model, Integer weight, String regNo, Depot depot, Integer liquidCapacity, String liquidType) {
 		super(make, model, weight, regNo, depot);
 
 		this.liquidType = liquidType;
